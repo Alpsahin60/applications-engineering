12cf10c2527ceb2ed78ce1ac97131495fa56353b
Alp <alpsahin60@outlook.com>
2026-06-17 13:07:22 +0200

    chore: Angular-Workspace mit Web- und Mobile-App initialisieren

---
+++ projects/weather-core/src/public-api.ts
@@ -0,0 +1,7 @@
+export * from './lib/weather-api.config';
+export * from './lib/models/openweather-response';
+export * from './lib/models/weather';
+export * from './lib/models/weather-error';
+export * from './lib/storage/storage.service';
+export * from './lib/storage/last-location.store';
+export * from './lib/weather/weather.service';
